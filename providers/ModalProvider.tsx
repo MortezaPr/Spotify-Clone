aa8e16015339c8915522707e2b4feca4bfcd6285
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2023-09-04 12:46:33 +0330

    add upload song functionality

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -17,6 +18,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   );
 };
