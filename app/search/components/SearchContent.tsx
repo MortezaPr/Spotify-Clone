3fe34f6dcdc31d0fe69f98f106c44974ca94f609
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2023-09-08 18:18:47 +0330

    add search functionality

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import MediaItem from "@/components/MediaItem";
+import { Song } from "@/types";
+import React from "react";
+
+interface SearchContentProps {
+  songs: Song[];
+}
+
+const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  if (songs.length === 0) {
+    return <div className="w-full px-8 text-neutral-400">No songs found !</div>;
+  }
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-8">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <MediaItem onClick={() => {}} data={song} />
+          {/* TODO: Add Like Button Here */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchContent;
