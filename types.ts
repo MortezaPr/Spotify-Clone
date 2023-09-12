c01013029cd0815feb947095ca3da2fc74bcb804
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2023-09-12 15:01:40 +0330

    update types

--- types.ts
+++ types.ts
@@ -6,3 +6,48 @@ export interface Song {
   song_path: string;
   image_path: string;
 }
+
+export interface Product {
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+}
+
+export interface Price {
+  id: string;
+  product_id?: string;
+  active?: boolean;
+  description?: string;
+  unit_amount?: number;
+  currency?: string;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  products?: Product;
+}
+
+export interface UserDetails {
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+}
+
+export interface Subscription {
+  id: string;
+  user_id: string;
+  price_id?: string;
+  quantity?: number;
+  cancel_at_period_end?: boolean;
+  created: string;
+  current_period_start: string;
+  current_period_end: string;
+  ended_at?: string;
+  cancel_at?: string;
+  canceled_at?: string;
+  trial_start?: string;
+  trial_end?: string;
+  prices?: Price;
+}
